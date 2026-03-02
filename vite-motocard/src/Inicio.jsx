8d569b2330d2b37270cfacc21e15715a9d65a174
Rgadielbg <240349@utxicotepec.edu.mx>
2026-03-01 23:27:09 -0600

    segundo intento

--- vite-motocard/src/Inicio.jsx
+++ vite-motocard/src/Inicio.jsx
@@ -1,10 +1,40 @@
+import './Inicio.css'
+import Carro1 from './assets/carro1.png';
 
 
-function Inicio() { 
+
+
+
+function Inicio() {
     return (
-        <div className='inicioDiv'>
-            <h2>Bienvenido a MotorCard</h2>     
-        </div>
+
+        <>
+            <div className='inicioDiv'>
+                <h2>En MotorCard siempre lo mejor</h2>
+            </div>
+            <TarjetaComponent />
+
+
+            
+        </>
+
     )
+
 }
-    export default Inicio
+
+function TarjetaComponent() {
+  return (
+    <>
+   
+    <div className='TarjetaDiv'>
+      <img src={Carro1} alt='Logotipo' />
+      <h3>Coca-Cola</h3>
+      <p>Hola, A ver si me quiere salir este </p>
+
+    </div>
+    </>
+  );
+}
+
+
+export default Inicio
